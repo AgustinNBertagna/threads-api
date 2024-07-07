0dd233c63ee3f96191772497596f4ab0d98f71c2
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-06 21:33:45 -0300

    source code

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Elysia } from "elysia";
+import {
+  fetchFeed,
+  fetchSearch,
+  fetchPost,
+  fetchUserProfile,
+  fetchUserPosts,
+} from "./utils/fetch";
+
+const app = new Elysia()
+  .get("/api/feed", ({ query: { country, quantity } }) =>
+    fetchFeed(country, quantity)
+  )
+  .get("/api/user/id/:userID", ({ params: { userID } }) =>
+    fetchUserProfile({ userID })
+  )
+  .get("/api/user/username/:username", ({ params: { username } }) =>
+    fetchUserProfile({ username })
+  )
+  .get(
+    "api/user/posts/:userID",
+    ({ params: { userID }, query: { quantity } }) =>
+      fetchUserPosts(userID, quantity)
+  )
+  .get("/api/search", ({ query: { query, quantity } }) =>
+    fetchSearch(query, quantity)
+  )
+  .get("api/post/:postID", ({ params: { postID } }) => fetchPost(postID))
+  .listen(3000);
+
+console.log(`Server is running at ${app.server?.url}`);
---
+++ src/utils/fetch.ts
@@ -0,0 +1,67 @@
+import {
+  URL,
+  METHOD,
+  HEADERS,
+  DOCUMENTS_ID,
+  FB_LSD,
+  VARIABLES,
+} from "./constants";
+
+async function baseFetch({ doc_id, variables }: any) {
+  const response = await fetch(URL, {
+    method: METHOD,
+    headers: HEADERS,
+    body: new URLSearchParams({
+      doc_id,
+      lsd: FB_LSD,
+      variables: JSON.stringify({ ...VARIABLES, ...variables }),
+    }),
+  });
+  const json = await response.json();
+  return json;
+}
+
+export async function fetchUserProfile({ username = "", userID = "" }) {
+  const { userProfileByName, userProfileByID } = DOCUMENTS_ID;
+  const doc_id = username ? userProfileByName : userProfileByID;
+  const response = await baseFetch({
+    doc_id,
+    variables: { username, userID },
+  });
+  return response;
+}
+
+export async function fetchPost(postID: string) {
+  const response = await baseFetch({
+    doc_id: DOCUMENTS_ID.userPost,
+    variables: { postID },
+  });
+  return response;
+}
+
+export async function fetchFeed(
+  specified_country: string = "US",
+  first: string = "10"
+) {
+  const response = await baseFetch({
+    doc_id: DOCUMENTS_ID.feed,
+    variables: { specified_country, first },
+  });
+  return response;
+}
+
+export async function fetchSearch(query: string = "", first: string = "") {
+  const response = await baseFetch({
+    doc_id: DOCUMENTS_ID.searchUser,
+    variables: { query, first },
+  });
+  return response;
+}
+
+export async function fetchUserPosts(userID: string = "", first: string = "") {
+  const response = await baseFetch({
+    doc_id: DOCUMENTS_ID.userPosts,
+    variables: { userID, first },
+  });
+  return response;
+}
