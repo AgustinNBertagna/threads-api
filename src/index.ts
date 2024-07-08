c2ccf685e195234a8f3c708e82ea5553c4e0226d
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-08 01:43:09 -0300

    Feat(Routes): added user responses and reposts endpoints

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,42 @@ import {
   fetchUserProfile,
   fetchUserPosts,
   fetchRelatedPosts,
+  fetchUserResponses,
+  fetchUserReposts,
 } from "./utils/fetch";
 
 const app = new Elysia()
   .get("/api/feed", ({ query: { country, quantity } }) =>
     fetchFeed(country, quantity)
   )
-  .get("/api/user/id/:userID", ({ params: { userID } }) =>
-    fetchUserProfile({ userID })
+  .get("/api/search", ({ query: { query, quantity } }) =>
+    fetchSearch(query, quantity)
   )
   .get("/api/user/username/:username", ({ params: { username } }) =>
     fetchUserProfile({ username })
   )
+  .get("/api/user/:userID", ({ params: { userID } }) =>
+    fetchUserProfile({ userID })
+  )
   .get(
-    "api/user/posts/:userID",
+    "/api/user/:userID/posts",
     ({ params: { userID }, query: { quantity } }) =>
       fetchUserPosts(userID, quantity)
   )
-  .get("/api/search", ({ query: { query, quantity } }) =>
-    fetchSearch(query, quantity)
+  .get(
+    "/api/user/:userID/responses",
+    ({ params: { userID }, query: { quantity } }) =>
+      fetchUserResponses(userID, quantity)
   )
-  .get("api/relatedposts/:targetPostID", ({ params: { targetPostID } }) =>
-    fetchRelatedPosts(targetPostID)
+  .get(
+    "/api/user/:userID/reposts",
+    ({ params: { userID }, query: { quantity } }) =>
+      fetchUserReposts(userID, quantity)
   )
   .get("api/post/:postID", ({ params: { postID } }) => fetchPost(postID))
+  .get("api/post/:postID/related", ({ params: { postID } }) =>
+    fetchRelatedPosts(postID)
+  )
   .listen(3000);
 
 console.log(`Server is running at ${app.server?.url}`);
