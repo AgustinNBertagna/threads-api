4b2b5da40aa56adc258fa0dcd5a46e5c6dcb0531
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-17 23:24:24 -0300

    UsersControler(Feat): Added Search User endpoint

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,9 +1,16 @@
 import { Elysia } from "elysia";
 import usersModels from "../models/usersModels";
-import { fetchUser, fetchUserSection } from "../services/usersService";
+import {
+  fetchUser,
+  fetchUserSearch,
+  fetchUserSection,
+} from "../services/usersService";
 
 export default new Elysia({ name: "users", prefix: "user" })
   .use(usersModels)
+  .get("search", ({ query }) => fetchUserSearch(query), {
+    query: "searchQuery",
+  })
   .get("username/:username", ({ params }) => fetchUser(params), {
     params: "usernameParams",
   })
