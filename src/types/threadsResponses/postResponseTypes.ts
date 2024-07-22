b4c0b898552204ba6a60038907b97d4a92e05d28
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-22 19:31:27 -0300

    Types(Feat): Added Posts responses types from threads api

---
+++ src/types/threadsResponses/postResponseTypes.ts
@@ -0,0 +1,222 @@
+export interface PostResponse {
+  data: MainData;
+  extensions: Extensions;
+}
+
+interface MainData {
+  data: DataData;
+}
+
+interface DataData {
+  edges: Edge[];
+  page_info: PageInfo;
+}
+
+interface Edge {
+  node: Node;
+  cursor: string;
+}
+
+interface Node {
+  thread_header_context: null;
+  thread_items: ThreadItem[];
+  thread_type: ThreadType;
+  header: null;
+  id: string;
+  __typename: NodeTypename;
+}
+
+enum NodeTypename {
+  XDTThread = "XDTThread",
+}
+
+interface ThreadItem {
+  post: Post;
+  reply_facepile_users: any[];
+  line_type: LineType;
+  should_show_replies_cta: boolean;
+}
+
+enum LineType {
+  Line = "line",
+  None = "none",
+}
+
+interface Post {
+  pk: string;
+  user: User;
+  text_post_app_info: TextPostAppInfo;
+  id: string;
+  is_paid_partnership: null;
+  is_fb_only: null;
+  is_internal_only: null;
+  media_overlay_info: null;
+  sharing_friction_info: SharingFrictionInfo;
+  code: string;
+  carousel_media: null;
+  image_versions2: ImageVersions2;
+  original_height: number;
+  original_width: number;
+  video_versions: VideoVersion[] | null;
+  caption: Caption | null;
+  like_count: number;
+  audio: null;
+  caption_is_edited: boolean;
+  transcription_data: null;
+  accessibility_caption: null | string;
+  has_audio: boolean | null;
+  media_type: number;
+  has_liked: boolean;
+  caption_add_on: null;
+  text_with_entities: TextWithEntities | null;
+  giphy_media_info: null;
+  meta_place: null;
+  taken_at: number;
+  organic_tracking_token: string;
+  like_and_view_counts_disabled: boolean;
+  fbid: string;
+}
+
+interface Caption {
+  pk: string;
+  text: string;
+  has_translation: null;
+}
+
+interface ImageVersions2 {
+  candidates: Candidate[];
+}
+
+interface Candidate {
+  height: number;
+  url: string;
+  width: number;
+}
+
+interface SharingFrictionInfo {
+  should_have_sharing_friction: boolean;
+  sharing_friction_payload: null;
+}
+
+interface TextPostAppInfo {
+  is_post_unavailable: boolean;
+  pinned_post_info: PinnedPostInfo;
+  share_info: ShareInfo;
+  reply_to_author: ReplyToAuthor | null;
+  direct_reply_count: number;
+  repost_count: number;
+  hush_info: null;
+  __typename: TextPostAppInfoTypename;
+  can_reply: boolean;
+  special_effects_enabled_str: string;
+  quote_count: number;
+  text_fragments: TextFragments;
+  is_reply: boolean;
+  link_preview_attachment: null;
+  fediverse_info: null;
+  post_unavailable_reason: null;
+}
+
+enum TextPostAppInfoTypename {
+  XDTTextPostAppMediaInfo = "XDTTextPostAppMediaInfo",
+}
+
+interface PinnedPostInfo {
+  is_pinned_to_profile: boolean;
+  is_pinned_to_parent_post: boolean;
+}
+
+interface ReplyToAuthor {
+  username: Username;
+  id: null;
+}
+
+enum Username {
+  GinaPietras777 = "gina.pietras777",
+  Mrkermitmeow = "mrkermitmeow",
+}
+
+interface ShareInfo {
+  reposted_post: null;
+  quoted_post: null;
+  can_quote_post: boolean;
+  can_repost: boolean;
+  is_reposted_by_viewer: boolean;
+  repost_restricted_reason: RepostRestrictedReason;
+}
+
+enum RepostRestrictedReason {
+  Private = "private",
+}
+
+interface TextFragments {
+  fragments: Fragment[];
+}
+
+interface Fragment {
+  fragment_type: FragmentType;
+  link_fragment: null;
+  mention_fragment: null;
+  plaintext: string;
+  tag_fragment: TagFragment | null;
+}
+
+enum FragmentType {
+  Plaintext = "plaintext",
+  TextAppTag = "text_app_tag",
+}
+
+interface TagFragment {
+  id: string;
+}
+
+interface TextWithEntities {
+  entities: EntityElement[];
+}
+
+interface EntityElement {
+  entity: EntityEntity;
+  offset: number;
+  length: number;
+}
+
+interface EntityEntity {
+  id: string;
+  display_text: string;
+  entity_type: string;
+}
+
+interface User {
+  friendship_status: null;
+  id: null;
+  pk: string;
+  eligible_for_text_app_activation_badge: boolean;
+  hide_text_app_activation_badge_on_text_app: boolean;
+  transparency_label: null;
+  transparency_product: null;
+  transparency_product_enabled: boolean;
+  is_verified: boolean;
+  username: string;
+  profile_pic_url: string;
+  text_post_app_is_private: boolean;
+}
+
+interface VideoVersion {
+  type: number;
+  url: string;
+}
+
+enum ThreadType {
+  Thread = "thread",
+}
+
+interface PageInfo {
+  has_next_page: boolean;
+  has_previous_page: boolean;
+  end_cursor: null;
+  start_cursor: null;
+}
+
+interface Extensions {
+  is_final: boolean;
+}
---
+++ src/types/threadsResponses/relatedPostsResponseTypes.ts
@@ -0,0 +1,231 @@
+export interface RelatedPostsResponse {
+  data: Data;
+  extensions: Extensions;
+}
+
+export interface Data {
+  relatedPosts: RelatedPosts;
+}
+
+export interface RelatedPosts {
+  threads: Thread[];
+}
+
+export interface Thread {
+  id: string;
+  thread_header_context: null;
+  thread_items: ThreadItem[];
+  thread_type: string;
+  header: null;
+}
+
+export interface ThreadItem {
+  post: Post;
+  reply_facepile_users: ReplyFacepileUser[];
+  line_type: string;
+  should_show_replies_cta: boolean;
+}
+
+export interface Post {
+  pk: string;
+  user: User;
+  text_post_app_info: PostTextPostAppInfo;
+  id: string;
+  is_paid_partnership: null;
+  is_fb_only: null;
+  is_internal_only: null;
+  media_overlay_info: null;
+  sharing_friction_info: SharingFrictionInfo;
+  code: string;
+  carousel_media: null;
+  image_versions2: ImageVersions2;
+  original_height: number;
+  original_width: number;
+  video_versions: VideoVersion[] | null;
+  caption: Caption | null;
+  like_count: number;
+  audio: null;
+  caption_is_edited: boolean;
+  transcription_data: null;
+  accessibility_caption: null | string;
+  has_audio: boolean | null;
+  media_type: number;
+  has_liked: boolean;
+  caption_add_on: null;
+  text_with_entities: TextWithEntities | null;
+  giphy_media_info: null;
+  meta_place: null;
+  taken_at: number;
+  organic_tracking_token: string;
+  like_and_view_counts_disabled: boolean;
+  fbid: string;
+}
+
+export interface Caption {
+  pk: string;
+  text: string;
+  has_translation: null;
+}
+
+export interface ImageVersions2 {
+  candidates: Candidate[];
+}
+
+export interface Candidate {
+  height: number;
+  url: string;
+  width: number;
+}
+
+export interface SharingFrictionInfo {
+  should_have_sharing_friction: boolean;
+  sharing_friction_payload: null;
+}
+
+export interface PostTextPostAppInfo {
+  is_post_unavailable: boolean;
+  pinned_post_info: null;
+  share_info: PurpleShareInfo;
+  reply_to_author: null;
+  direct_reply_count: number;
+  repost_count: number;
+  hush_info: null;
+  __typename: string;
+  can_reply: boolean;
+  special_effects_enabled_str: string;
+  quote_count: number;
+  text_fragments: TextFragments;
+  is_reply: boolean;
+  link_preview_attachment: null;
+  fediverse_info: null;
+  post_unavailable_reason: null;
+}
+
+export interface PurpleShareInfo {
+  reposted_post: null;
+  quoted_post: QuotedPost | null;
+  can_quote_post: boolean;
+  can_repost: boolean;
+  is_reposted_by_viewer: boolean;
+  repost_restricted_reason: string;
+}
+
+export interface QuotedPost {
+  is_paid_partnership: null;
+  carousel_media: null;
+  code: string;
+  image_versions2: ImageVersions2;
+  original_height: number;
+  original_width: number;
+  video_versions: null;
+  caption: Caption;
+  pk: string;
+  user: User;
+  audio: null;
+  caption_is_edited: boolean;
+  transcription_data: null;
+  accessibility_caption: string;
+  has_audio: null;
+  media_type: number;
+  caption_add_on: null;
+  text_post_app_info: QuotedPostTextPostAppInfo;
+  text_with_entities: null;
+  is_fb_only: null;
+  is_internal_only: null;
+  media_overlay_info: null;
+  giphy_media_info: null;
+  taken_at: number;
+  like_count: number;
+  like_and_view_counts_disabled: boolean;
+  id: string;
+}
+
+export interface QuotedPostTextPostAppInfo {
+  special_effects_enabled_str: string;
+  text_fragments: TextFragments;
+  link_preview_attachment: null;
+  share_info: FluffyShareInfo;
+  direct_reply_count: number;
+  is_post_unavailable: boolean;
+}
+
+export interface FluffyShareInfo {
+  quoted_post_caption: null;
+}
+
+export interface TextFragments {
+  fragments: Fragment[];
+}
+
+export interface Fragment {
+  fragment_type: FragmentType;
+  link_fragment: null;
+  mention_fragment: MentionFragment | null;
+  plaintext: string;
+  tag_fragment: TagFragment | null;
+}
+
+export enum FragmentType {
+  Mention = "mention",
+  Plaintext = "plaintext",
+  TextAppTag = "text_app_tag",
+}
+
+export interface MentionFragment {
+  mentioned_user: MentionedUser;
+}
+
+export interface MentionedUser {
+  is_active_on_text_post_app: boolean;
+  username: string;
+  id: null;
+}
+
+export interface TagFragment {
+  id: string;
+}
+
+export interface User {
+  pk: string;
+  id: null;
+  username: string;
+  eligible_for_text_app_activation_badge: boolean;
+  hide_text_app_activation_badge_on_text_app: boolean;
+  friendship_status: null;
+  text_post_app_is_private: boolean;
+  profile_pic_url: string;
+  is_verified: boolean;
+  transparency_label?: null;
+  transparency_product?: null;
+  transparency_product_enabled?: boolean;
+}
+
+export interface TextWithEntities {
+  entities: EntityElement[];
+}
+
+export interface EntityElement {
+  entity: EntityEntity;
+  offset: number;
+  length: number;
+}
+
+export interface EntityEntity {
+  id: string;
+  display_text: string;
+  entity_type: string;
+}
+
+export interface VideoVersion {
+  type: number;
+  url: string;
+}
+
+export interface ReplyFacepileUser {
+  profile_pic_url: string;
+  id: null;
+}
+
+export interface Extensions {
+  is_final: boolean;
+}
