7ebd77529157c9ed8c3990f9f5ef68fc414161bd
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-22 17:49:21 -0300

    Feed Service(Feat): Added response transformer to remove not wanted data from responses

--- src/services/feedService.ts
+++ src/services/feedService.ts
@@ -1,15 +1,25 @@
 import threadsClient from "./threadsClient";
 import { Docs } from "../types/threadsClientTypes";
 import type { FeedQuery } from "../types/feedTypes";
+import type { FeedResponse } from "../types/threadsResponses/feedResponseTypes";
+
+function transformResponse(response: FeedResponse) {
+  const edges = response.data.feedData.edges;
+  const posts = edges.map((threadItem) => {
+    const post = threadItem.text_post_app_thread.thread_items[0].post;
+    return post;
+  });
+  return posts;
+}
 
 export async function fetchFeed({
   country: specified_country,
   quantity: first,
 }: FeedQuery) {
-  const response = await threadsClient({
+  const response: FeedResponse = await threadsClient({
     doc_id: Docs.FEED,
     specified_country,
     first,
   });
-  return response;
+  return transformResponse(response);
 }
