48929202b891f0de51bd67bfec169d5b3c6dac6b
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-22 19:30:57 -0300

    Posts Service (Feat): Added transform response to Post related services

--- src/services/postsService.ts
+++ src/services/postsService.ts
@@ -1,10 +1,28 @@
 import threadsClient from "./threadsClient";
 import { Docs } from "../types/threadsClientTypes";
 import type { PostsParams } from "../types/postsTypes";
+import type { PostResponse } from "../types/threadsResponses/postResponseTypes";
+import type { RelatedPostsResponse } from "../types/threadsResponses/relatedPostsResponseTypes";
+
+function transformPostResponse(response: PostResponse) {
+  const edges = response.data.data.edges;
+  const post = edges.splice(0, 1)[0].node.thread_items[0].post;
+  const replies = edges.map((edge) => edge.node.thread_items[0].post);
+  return { post, replies };
+}
+
+function transformRelatedPostsResponse(response: RelatedPostsResponse) {
+  const threads = response.data.relatedPosts.threads;
+  const posts = threads.map((thread) => thread.thread_items[0].post);
+  return posts;
+}
 
 export async function fetchPost({ postID }: PostsParams) {
-  const response = await threadsClient({ doc_id: Docs.POST, postID });
-  return response;
+  const response: PostResponse = await threadsClient({
+    doc_id: Docs.POST,
+    postID,
+  });
+  return transformPostResponse(response);
 }
 
 export async function fetchRelatedPosts({ postID }: PostsParams) {
@@ -12,5 +30,5 @@ export async function fetchRelatedPosts({ postID }: PostsParams) {
     doc_id: Docs.RELATED_POSTS,
     targetPostID: postID,
   });
-  return response;
+  return transformRelatedPostsResponse(response);
 }
