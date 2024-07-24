78d8bb57f8470ff2d7b771362cc99303e2846aa9
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-23 21:39:26 -0300

    ErrorHandler(Feat): Added Unauthorized custom error class and error handling

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,19 +1,36 @@
 import { Elysia } from "elysia";
 
-export default new Elysia().onError({ as: "global" }, ({ code, error }) => {
-  if (code === "NOT_FOUND") {
-    const message = "Resource not found";
-    const { message: _, ...rest } = error;
-    return { message, ...rest };
+export class UnauthorizedError extends Error {
+  public status: number = 401;
+  public code: string = "UNAUTHORIZED";
+  constructor(message?: string) {
+    super(message);
   }
+}
 
-  if (code === "VALIDATION") {
-    const message = "Unprocessable Entity";
-    const { message: _, all, code, status } = error;
-    const errors: any = {};
-    all.forEach((err, indx) => {
-      errors[`error${all.length === 1 ? "" : indx + 1}`] = err.schema.error;
-    });
-    return { message, errors, code, status };
-  }
-});
+export default new Elysia()
+  .error({ UNAUTHORIZED: UnauthorizedError })
+  .onError({ as: "global" }, ({ code, error }) => {
+    console.log(code);
+    const errorMsg = error.message;
+    if (code === "NOT_FOUND") {
+      const message =
+        errorMsg !== "NOT_FOUND" ? errorMsg : "Resource Not Found";
+      const status = error.status;
+      return { message, status, code };
+    }
+
+    if (code === "UNAUTHORIZED") {
+      const { message, status } = error;
+      return { message, status, code };
+    }
+
+    if (code === "VALIDATION") {
+      const { message, all, status } = error;
+      const errors: any = {};
+      all.forEach((err, indx) => {
+        errors[`error${all.length === 1 ? "" : indx + 1}`] = err.schema.error;
+      });
+      return { message, errors, code, status };
+    }
+  });
