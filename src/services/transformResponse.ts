5e58230a3650d712e2051d870b4ba18791c9c781
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-23 22:10:21 -0300

    Transform Responses(Fix): Added error handling cases

--- src/services/transformResponse.ts
+++ src/services/transformResponse.ts
@@ -1,3 +1,5 @@
+import { NotFoundError } from "elysia";
+import { UnauthorizedError } from "../middlewares/errorHandler";
 import type { UserResponse } from "../types/threadsResponses/userTypes";
 import type { FeedResponse } from "../types/threadsResponses/feedTypes";
 import type { PostResponse } from "../types/threadsResponses/postTypes";
@@ -16,6 +18,7 @@ export function transformFeedResponse(response: FeedResponse) {
 }
 
 export function transformPostResponse(response: PostResponse) {
+  if (!response.data) throw new NotFoundError("Post not found");
   const edges = response.data.data.edges;
   const post = edges.splice(0, 1)[0].node.thread_items[0].post;
   const replies = edges.map((edge) => edge.node.thread_items[0].post);
@@ -23,6 +26,7 @@ export function transformPostResponse(response: PostResponse) {
 }
 
 export function transformRelatedPostsResponse(response: RelatedPostsResponse) {
+  if (!response.data) throw new NotFoundError("Post not found");
   const threads = response.data.relatedPosts.threads;
   const posts = threads.map((thread) => thread.thread_items[0].post);
   return posts;
@@ -30,15 +34,19 @@ export function transformRelatedPostsResponse(response: RelatedPostsResponse) {
 
 export function transformSearchUserResponse(response: SearchUserResponse) {
   const edges = response.data.xdt_api__v1__users__search_connection.edges;
+  if (!edges.length) throw new NotFoundError("No users were found");
   const users = edges.map((edge) => edge.node);
   return users;
 }
 
 export function transformUserResponse(response: UserResponse) {
-  return response.data.user;
+  const user = response.data.user;
+  if (!user) throw new NotFoundError("User not found");
+  return user;
 }
 
 export function transformUserPostsResponse(response: UserPostsResponse) {
+  if (!response.data) throw new UnauthorizedError("User has a private account");
   const edges = response.data.mediaData.edges;
   const posts = edges.map((edge) => edge.node.thread_items[0].post);
   return posts;
@@ -47,6 +55,7 @@ export function transformUserPostsResponse(response: UserPostsResponse) {
 export function transformUserResponsesResponse(
   response: UserResponsesResponse
 ) {
+  if (!response.data) throw new UnauthorizedError("User has a private account");
   const edges = response.data.mediaData.edges;
   const responses = edges.map((edge) =>
     edge.node.thread_items.map((thread) => thread.post)
@@ -55,6 +64,7 @@ export function transformUserResponsesResponse(
 }
 
 export function transformUserRepostsResponse(response: UserRepostsResponse) {
+  if (!response.data) throw new UnauthorizedError("User has a private account");
   const edges = response.data.mediaData.edges;
   const reposts = edges.map((edge) => edge.node.thread_items[0].post);
   return reposts;
