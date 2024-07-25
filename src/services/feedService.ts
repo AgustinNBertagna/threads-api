01a6fb8a46f6a451128838209fa0cb5f2b01cf38
AgustinN. Bertagna <agus2713@hotmail.com>
2024-07-25 02:23:18 -0300

    Feed Service(Refactor): Small refactor

--- src/services/feedService.ts
+++ src/services/feedService.ts
@@ -4,14 +4,11 @@ import { transformFeedResponse } from "./transformResponse";
 import type { FeedQuery } from "../types/feedTypes";
 import type { FeedResponse } from "../types/threadsResponses/feedTypes";
 
-export async function fetchFeed({
-  country: specified_country,
-  quantity: first,
-}: FeedQuery) {
+export async function fetchFeed({ country, quantity }: FeedQuery) {
   const response: FeedResponse = await threadsClient({
     doc_id: Docs.FEED,
-    specified_country,
-    first,
+    specified_country: country,
+    first: quantity,
   });
   return transformFeedResponse(response);
 }
